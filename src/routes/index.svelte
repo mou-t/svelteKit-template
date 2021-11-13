31e661730062904c15b5eceddd03acc4e8d3c509
mou-t <detective0218gm@gmail.com>
2021-11-14 01:27:09 +0900

    add .vscode/setting.json and run format

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
 <h1 class="text-3xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev" class="link link-hover">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Visit <a href="https://kit.svelte.dev" class="link link-hover">kit.svelte.dev</a> to read the documentation
+</p>
